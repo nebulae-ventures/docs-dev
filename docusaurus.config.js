00354501f8f201dd8619be1ad343dd6ffbc387ac
Louis <larotu00@gmail.com>
2022-01-31 15:41:30 +0000

    essai1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://nebulae-ventures.github.io/docs/',
-  baseUrl: '/',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
