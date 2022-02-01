cc057edb3dda9749ae948f33179335c037bede4a
Louis <larotu00@gmail.com>
2022-02-01 02:28:35 +0100

    try github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://nebulae-ventures.github.io/docs-dev/',
-  baseUrl: '/',
+  baseUrl: '/docs-dev/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
